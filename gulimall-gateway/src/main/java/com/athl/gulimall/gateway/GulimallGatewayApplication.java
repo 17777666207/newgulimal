bf75144e7bdc95a52860ba41cdf059535aef0d6e
phandaai <907230569@qq.com>
2022-08-06 13:08:36 +0800

    first commit

---
+++ gulimall-gateway/src/main/java/com/athl/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.athl.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 1.开启注册发现
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) // 排除数据库相关的配置
+@EnableDiscoveryClient
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
