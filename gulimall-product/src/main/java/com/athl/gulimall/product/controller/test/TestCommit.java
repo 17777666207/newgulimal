fc375a761a7a9bc5acee42f7279df223e6d555b8
phandaai <907230569@qq.com>
2023-04-15 23:40:34 +0800

    分支测试

---
+++ gulimall-product/src/main/java/com/athl/gulimall/product/controller/test/TestCommit.java
@@ -0,0 +1,13 @@
+package com.athl.gulimall.product.controller.test;
+
+public class TestCommit {
+
+
+
+    public void testCommit(){
+        System.out.println("test");
+        System.out.println("why");
+        System.out.println("ok");
+
+    }
+}
