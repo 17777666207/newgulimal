d78239235e50d1c063344bc206c654ad81f5daf9
phandaai <907230569@qq.com>
2023-04-16 00:24:31 +0800

    分支测试01

---
+++ gulimall-product/src/main/java/com/athl/gulimall/product/controller/test/TestInterface.java
@@ -0,0 +1,9 @@
+package com.athl.gulimall.product.controller.test;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TestInterface {
+
+   public abstract String TestInterface();
+}
